9a097a7123b6f233c4f245fea73593e25037a311
Dave Waters <davewaters@live.ca>
2017-02-02 21:38:05 -0500

    Ran code through JSHint and beautifier

--- js/modal.js
+++ js/modal.js
@@ -6,25 +6,15 @@ var modalImg = document.getElementById("modal_image");
 
 _modal.style.display = "none";
 
-//A function to invoke pointer related function statements
-function pointer(func, element){
-    func(element);
-}
-
-//A function to invoke modal related function statements
-function modal(func) {
-    func(clear);
-}
-
 // On pointer hover set white close image.
 var hover = function(element) {
     element.setAttribute('src', 'Images/close_button_white.png');
-}
+};
 
 // When pointer is no long hovering set grey close image.
 var unhover = function(element) {
     element.setAttribute('src', 'Images/close_button_grey.png');
-}
+};
 
 // A function statement to be passed to the modal function when the user clicks the button.
 // The modal function will invoke this function to show the modal.
@@ -36,15 +26,15 @@ var showModal = function(message, src) {
 
     //Displaying the modal.
     _modal.style.display = "block";
-}
+};
 
 // A function statement to be passed to the modal function, to hide the modal.
 var close_modal = function(func) {
     func();
     _modal.style.display = "none";
-}
+};
 
 // A function statement to be passed to the modal function, to clear all previously set element values.
 var clear = function() {
     modalTitle.textContent = "";
-}
+};
